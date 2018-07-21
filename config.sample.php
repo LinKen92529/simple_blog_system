448decd52c5367b2e5b87ffe7d4e63ba4719a184
arasi27676271 <arasi27676271@gmail.com>
2018-07-21 21:25:06 +0800

    add top_array and fix style.css

--- config.sample.php
+++ config.sample.php
@@ -5,3 +5,4 @@ define('_DB_ID', '<Databse Id>');
 define('_DB_PW', '<Datbase password>');
 define('_DB_NAME', '<Database table name>');
 $admin_array = ['arasi27676271'];               //change the admin id you want
+$top_array = ['godarasi2767'];                 //change the top id you want
