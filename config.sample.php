03f29e0a9e9662055f0f864921f00e8d4707c239
arasi27676271 <arasi27676271@gmail.com>
2018-07-21 14:22:56 +0800

    add sample

---
+++ config.sample.php
@@ -0,0 +1,7 @@
+<?php
+define('_system_name', '<The system name>');
+define('_DB_HOST', '<Database host>');
+define('_DB_ID', '<Databse Id>');
+define('_DB_PW', '<Datbase password>');
+define('_DB_NAME', '<Database table name>');
+$admin_array = ['arasi27676271'];               //change the admin id you want
