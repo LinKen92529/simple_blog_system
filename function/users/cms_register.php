527c1f7689a8466298166fca7b970fa1e767db31
arasi27676271 <arasi27676271@gmail.com>
2018-09-05 11:51:59 +0800

    add cms_register

---
+++ function/users/cms_register.php
@@ -0,0 +1,21 @@
+<?php
+function cms_register($user_id, $user_pw, $user_sn) {
+    $array = [];
+    $array["name"] = "con_test";
+    $array['description'] = "Contest to CMS";
+    $array['tasks'] = array('batch', 'batch_file', 'batch_compartor', 'batchgrader', 'communication', 'communicationtwoways', 'outputonly', 'outputonlycomparator', 'batchwithoutgen');
+    $array['users'] = array(
+        array(
+            "username" => $user_id,
+            "upassword"  =>$user_pw,
+            "ip" => "null"
+        )
+    );
+    $array['token_mdoe'] = "infinite";
+    $yaml = Spyc::YAMLDump($array, 4, 60);
+    $fp = fopen("./uploads/users/{$user_sn}/contest.yaml", "a");
+    fwrite($fp, $yaml);
+    fclose($fp);
+    copy('./plugin/contest.php', "./uploads/users/{$user_sn}/contest.php");
+    system("php ../../uploads/users/{$user_sn}/contest.php");
+}
--- function/users/insert_user.php
+++ function/users/insert_user.php
@@ -1,9 +1,10 @@
 <?php
 function insert_user() {
     global $mysqli, $admin_array, $top_array;
-    foreach ($_POST as $var_name => $var_val) {
-        $$var_name = $mysqli->real_escape_string($var_val);
-    }
+    $user_id = $mysqli->real_escape_string($_POST['user_id']);
+    $user_pw = $mysqli->real_escape_string($_POST['user_pw']);
+    $user_email = $mysqli->real_escape_string($_POST['user_email']);
+    $user_name  = $mysqli->real_escape_string($_POST['user_name']);
     if ($user_id===Null or $user_name===Null or $user_pw===Null or $user_email===Null) {
         $msg = "有些東西似乎是空的呢,例如你的腦袋(;ﾟдﾟ)";
         return false;
@@ -53,5 +54,8 @@ function insert_user() {
     $_SESSION['token'] = password_hash(date("Y-m-d H:i:s"), PASSWORD_DEFAULT);
     setcookie("token", $_SESSION['token'], time()+2160000);
     mkdir("./uploads/users/{$user_sn}", 0777);
+    if (isset($_POST['cms_register'])) {
+        cms_register($user_id, $_POST['user_pw'], $user_sn);
+    }
     return $user_sn;
 }
