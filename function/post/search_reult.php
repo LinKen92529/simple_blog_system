83675cab9453e5f5d661a608d9d8b99f4c743b76
arasi27676271 <33758217+arasi27676271@users.noreply.github.com>
2019-02-20 11:14:36 +0800

    Update search_reult.php
    fixed search result

--- function/post/search_reult.php
+++ function/post/search_reult.php
@@ -1,19 +1,21 @@
 <?php
 function search_result($keyword) {
     global $mysqli, $smarty;
-    $sql = "SELECT * FROM `post` WHERE `post_title` LIKE '%{$keyword}%' OR (`post_tag` LIKE '%{$keyword}%')";
+    $sql = "SELECT * FROM `post` WHERE `post_title` LIKE '%{$keyword}%' OR `post_tag` LIKE '%{$keyword}%'";
     $result = $mysqli->query($sql) or die($mysqli->connect_error);
     $i = 0;
     while ($post = $result->fetch_assoc()) {
-        $tag_array = explode(';', $post['post_tag']);
+        if ($post['post_display'] == 'hide') continue;
         $find_result[$i] = $post;
+        if (strlen($find_result[$i]['post_title'])>15) {
+            $find_result[$i]['post_title'] = substr($find_result[$i]['post_title'], 0, 15) . '...';
+        }
         $find_result[$i]['pic'] = get_pic_path("./uploads/post/{$post['post_sn']}/normal_post_pic.png", "./img/normal_get_pic.jpg");
-        $find_result[$i]['post_tag'] = $tag_array;
         // find post owner
         $user_sn = $post['post_owner'];
         $sql = "SELECT `user_name` FROM `users` WHERE `user_sn`='{$user_sn}'";
-        $result = $mysqli->query($sql) or die($mysqli->connect_error);
-        if ($user = $result->fetch_assoc()) {
+        $rresult = $mysqli->query($sql) or die($mysqli->connect_error);
+        if ($user = $rresult->fetch_assoc()) {
             $find_result[$i]['post_owner'] = $user['user_name'];
         } else {
             $find_result[$i]['post_owner'] = '不明';
