187a262f1beeee9ca714213e78c613ff72631bd0
arasi27676271 <33758217+arasi27676271@users.noreply.github.com>
2019-03-10 20:15:43 +0800

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('_system_name', 'TFcis20 NoteWeb');
-define('_DB_HOST', 'localhost');
-define('_DB_ID', 'root');
-define('_DB_PW', 'tfcis.test');
-define('_DB_NAME', 'arashi');
-$admin_array = [];
-$top_array = ['arasi27676271', 'godarasi2767'];
