a46f96ccb3292845800e62f0fa1bb4a8fa29e5ab
arasHi_ <33758217+arasi27676271@users.noreply.github.com>
2019-03-16 10:26:39 +0800

    Update webset_bg.php
    fix bg update

--- function/webset/webset_bg.php
+++ function/webset/webset_bg.php
@@ -1,8 +1,8 @@
 <?php
 function webset_bg($file) {
-    if ($file['type'] != 'image/jpeg') {
-        die('請上傳副檔名為jpg或jpeg的檔案(￣(エ)￣)');
+    if ($file['type'] != 'image/png') {
+        die('請上傳副檔名為png的檔案(￣(エ)￣)');
     }
-    delete_file("img/bg.jpg", "file");
-    move_uploaded_file($file['tmp_name'], "img/bg.jpg");
+    delete_file("img/bg.png", "file");
+    move_uploaded_file($file['tmp_name'], "img/bg.png");
 }
